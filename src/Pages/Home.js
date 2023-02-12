6a02c0d33f25ba1cc7d15bd96ae33df22af1eb38
Mamun Or Rashid <86666104+Mamun35421@users.noreply.github.com>
2023-02-13 04:21:05 +0600

    Home-section add

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,76 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <section className='home-wrapper-1 py-5'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-6'>
+              <div className='main-banner position-relative '>
+                <img src='images/main-banner-1.jpg' className='img-fluid rounded-3' alt='main-banner' />
+
+                <div className='main-banner-content position-absolute'>
+                  <h4>SUPERCHARGED FOR PROS</h4>
+                  <h5>ipad s13+ Pro.</h5>
+                  <p>FROM $999.00 or $41.62</p>
+                  <Link className='button'>BUY NOW</Link>
+                </div>
+              </div>
+            </div>
+
+            <div className='col-6'>
+              <div className='d-flex flex-wrap justify-content-between gap-10 align-items-center'>
+                <div className='small-banner position-relative '>
+                  <img src='images/catbanner-01.jpg' className='img-fluid rounded-3' alt='main-banner' />
+
+                  <div className='small-banner-content position-absolute'>
+                    <h4>NEW ARRIVAL</h4>
+                    <h5>ipad s13+ Pro.</h5>
+                    <p>FROM $999.00 <br/> or $41.62</p>
+
+                  </div>
+                </div>
+                <div className='small-banner position-relative '>
+                  <img src='images/catbanner-02.jpg' className='img-fluid rounded-3' alt='main-banner' />
+
+                  <div className='small-banner-content position-absolute'>
+                    <h4>NEW ARRIVAL</h4>
+                    <h5>ipad s13+ Pro.</h5>
+                    <p>FROM $999.00 <br/> or $41.62</p>
+
+                  </div>
+                </div>
+                <div className='small-banner position-relative '>
+                  <img src='images/catbanner-03.jpg' className='img-fluid rounded-3' alt='main-banner' />
+
+                  <div className='small-banner-content position-absolute'>
+                    <h4>Best sale</h4>
+                    <h5>ipad s13+ Pro.</h5>
+                    <p>FROM $999.00 <br/> or $41.62</p>
+
+                  </div>
+                </div>
+                <div className='small-banner position-relative '>
+                  <img src='images/catbanner-04.jpg' className='img-fluid rounded-3' alt='main-banner' />
+
+                  <div className='small-banner-content position-absolute'>
+                    <h4>NEW ARRIVAL</h4>
+                    <h5>bY ipad here</h5>
+                    <p>FROM $999.00 <br/> or $41.62</p>
+
+                  </div>
+                </div>
+
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+      </section>
+    </>
   )
 }
 
